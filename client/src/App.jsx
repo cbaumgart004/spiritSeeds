1aa6e3ad899b55d911a6c94032f172af5794f7f6
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-10-06 12:16:46 -0600

    merge conflicts resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,12 +15,6 @@ import { MantineProvider } from "@mantine/core";
 
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { MantineProvider } from '@mantine/core';
-import '@mantine/core/styles.css';
-import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
 
 const httpLink = createHttpLink({
   uri: "/graphql",
