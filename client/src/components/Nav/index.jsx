44dd1f3d59bfc9c4c6c5456760cd63a25229fcc0
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-06 12:28:50 -0600

    Added Hamburger to Nav bar

--- client/src/components/Nav/index.jsx
+++ client/src/components/Nav/index.jsx
@@ -1,11 +1,12 @@
 /* eslint-disable no-unused-vars */
 
-import React from 'react'
-
+import React, { useState } from 'react'
+import { Burger, Drawer, ScrollArea } from '@mantine/core'
 import Auth from '../../utils/auth'
 import { Link } from 'react-router-dom'
 
 const Nav = () => {
+  const [opened, setOpened] = useState(false)
   function showNavigation() {
     if (Auth.loggedIn()) {
       return (
@@ -40,6 +41,9 @@ const Nav = () => {
           <Link to="/Calendar">Calendar</Link>
         </li> */}
           <li className="mx-1">
+            <Link to="/values">Values</Link>
+          </li>
+          <li className="mx-1">
             <Link to="/Merch">Merch</Link>
           </li>
           <li className="mx-1">
@@ -59,8 +63,27 @@ const Nav = () => {
         <h1 className="site-title">
           <Link to="/">Home</Link>
         </h1>
-
-        <nav>{showNavigation()}</nav>
+        <Burger
+          color="white"
+          size="md"
+          variant="solid"
+          className="burger-icon"
+          onClick={() => setOpened((o) => !o)}
+          opened={opened}
+          ripple="light"
+        />
+        &#9776; {/* This represents the hamburger icon */}
+        <Drawer
+          opened={opened}
+          onClose={() => setOpened(false)}
+          title="Menu"
+          padding="md"
+          size="sm"
+        >
+          <ScrollArea style={{ height: 'calc(20vh - 60px)' }}>
+            {showNavigation()}
+          </ScrollArea>
+        </Drawer>
       </div>
     </header>
   )
