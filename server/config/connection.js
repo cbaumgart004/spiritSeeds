473ed112c793875f61240e943da2d8ea8b7538ff
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-10-07 19:50:43 -0600

    mongodb stripe functionality

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/mern-shopping');
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/spiritseeds"
+);
+
+// const db =
+//   "mongodb+srv://admin001:KqCDS97LdScuk61h@cluster0.abfz5.mongodb.net/admin001?retryWrites=true&w=majority&appName=Cluster0";
+// mongoose.connect(db);
 
 module.exports = mongoose.connection;
