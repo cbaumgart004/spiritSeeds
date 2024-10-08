46b7645fc7ca7d2e3acaadf1cae3f65a0cdb13a2
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-07 20:21:39 -0600

    added services

--- client/src/components/ServicesProvided/index.jsx
+++ client/src/components/ServicesProvided/index.jsx
@@ -4,76 +4,18 @@ import React, { useEffect, useState } from 'react'
 import { Container, Title, Text, Button } from '@mantine/core'
 import classes from '@/assets/css/HeroImageRight.module.css'
 import { Link } from 'react-router-dom'
+import Abhyanga from './Abhyanga'
+import ChiNeSang from './ChiNeSang'
+import IntegrativeHealing from './IntegrativeHealing'
+import ThaiCompress from './ThaiCompress'
 
 const ServicesProvided = () => {
-  const [scrollY, setScrollY] = useState(0)
-
-  const handleScroll = () => {
-    setScrollY(window.scrollY)
-  }
-
-  useEffect(() => {
-    window.addEventListener('scroll', handleScroll)
-    return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  }, [])
-
   return (
-    <div
-      className={classes.root}
-      style={{
-        backgroundImage: `url('../assets/images/SoundHealing.JPG')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        height: '80vh',
-        position: 'relative', // To contain absolutely positioned elements
-      }}
-    >
-      <Container size="lg" style={{ position: 'relative', zIndex: 1 }}>
-        <div className={classes.inner}>
-          <div
-            className={classes.content}
-            style={{
-              transform: `translateY(${scrollY * -0.25}px)`, // Gives the content a parallax effect per M's request
-              marginTop: '300px', // Add margin at the top
-            }}
-          >
-            <Title className={classes.title}>
-              <span className={classes.gradientText}>
-                Spirit Seeds Wellness
-              </span>{' '}
-              Services
-            </Title>
-
-            <Text className={classes.description} mt={30}>
-              At Spirit Seeds Wellness, we provide a number of massage, yoga,
-              and sound healing services
-            </Text>
-            <Link to="/">
-              <Button
-                variant="gradient"
-                size="xl"
-                className={classes.control}
-                mt={40}
-              >
-                Home
-              </Button>
-            </Link>
-            <Link to="/BookService">
-              <Button
-                variant="gradient"
-                size="xl"
-                className={classes.control}
-                mt={40}
-              >
-                Book Now
-              </Button>
-            </Link>
-          </div>
-        </div>
-      </Container>
+    <div>
+      <IntegrativeHealing />
+      <Abhyanga />
+      <ChiNeSang />
+      <ThaiCompress />
     </div>
   )
 }
