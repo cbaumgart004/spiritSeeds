54466940aa8e62a1a86c1f02c148d87eadcfc4f9
AshLCorse <131395652+AshLCorse@users.noreply.github.com>
2024-10-17 18:00:56 -0600

    render prep

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 
 //when deploying, comment out 5-7, and uncomment 9-11
 
-// mongoose.connect(
-//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/spiritseeds"
-// );
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/spiritseeds"
+);
 
-const db =
-  "mongodb+srv://admin001:KqCDS97LdScuk61h@cluster0.abfz5.mongodb.net/admin001?retryWrites=true&w=majority&appName=Cluster0";
-mongoose.connect(db);
+// const db =
+//   "mongodb+srv://admin001:KqCDS97LdScuk61h@cluster0.abfz5.mongodb.net/admin001?retryWrites=true&w=majority&appName=Cluster0";
+// mongoose.connect(db);
 
 module.exports = mongoose.connection;
