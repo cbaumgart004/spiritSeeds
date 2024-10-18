c8f6dd3244afeb1493667c25b7c0357e1262f63d
Cbaumgart004 <baumgart.c@gmail.com>
2024-10-18 12:36:26 -0600

    Finalized README

--- client/src/pages/BookService.jsx
+++ client/src/pages/BookService.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react'
 import { DateTimePicker as MantineDatePicker } from '@mantine/dates'
 import { Select } from '@mantine/core'
